4c95e481d045cf7f165aaade83b8c4bca72a4baf
Eperson <epersoncmxf@gmail.com>
2025-04-18 23:44:35 -0300

    task entity

--- src/modules/tasks/entities/task.entity.ts
+++ src/modules/tasks/entities/task.entity.ts
@@ -13,11 +13,15 @@ export class Task extends EntityMutable {
   @JsonProperty()
   private description: string; // HTML content
 
+  @JsonProperty()
+  private ownerIds: string[];
+
   constructor(params: {
     id?: string;
     swimlaneId: string;
     name: string;
     description: string;
+    ownerIds?: string[];
     createdAtUtc?: Date;
     updatedAtUtc?: Date;
   }) {
@@ -26,12 +30,42 @@ export class Task extends EntityMutable {
     this.swimlaneId = params.swimlaneId;
     this.name = params.name;
     this.description = params.description;
+    this.ownerIds = params.ownerIds ?? [];
   }
 
   public getSwimlaneId(): string {
     return this.swimlaneId;
   }
+  
+  public getName(): string {
+    return this.name;
+  }
 
+  public getDescription(): string {
+    return this.description;
+  }
 
+  public getOwnerIds(): string[] {
+    return this.ownerIds;
+  }
+
+  public setName(name: string): Task {
+    this.name = name;
+    return this;
+  }
+
+  public setDescription(description: string) {
+    this.description = description;
+  }
+
+  public setSwimlaneId(swimlaneId: string): Task {
+    this.swimlaneId = swimlaneId;
+    return this;
+  }
+
+  public setOwnerIds(ownerIds: string[]): Task {
+    this.ownerIds = ownerIds;
+    return this;
+  }
 
 }
